5855e05cdc2d8023ac2ff32538fa08049858debb
justdanz <justdan2000@hotmail.com>
2016-12-01 14:37:49 -0500

    added getUserActivity. Stubbed out getAllSessions, and banUser

--- app/views/admin_v1.py
+++ app/views/admin_v1.py
@@ -1,5 +1,5 @@
 from app import app
-from app.models import User
+from app.models import User, Session, Score
 from app.utils import respond, check_params
 from datetime import datetime
 from flask import Blueprint, request
@@ -44,3 +44,79 @@ def v1_admin_getUser():
 		return respond("Unknown user", code=404)
 
 	return respond("SUCCESS", data={'user': user.getDict()})
+
+@admin_v1.route('/getUserActivityHistory', methods=['POST'])
+def v1_admin_getUserActivityHistory():
+	try:
+		check_params(request, ['key', 'uid'])
+		
+		if request.form['key'] != app.config['ADMIN_SECRET_KEY']:
+			raise StandardError('Bad. Go away.')
+
+		uid = request.form['uid']
+	except StandardError as e:
+		return respond(str(e), code=400), 400
+
+	user = User.query.filter(User.id == uid).first()
+
+	if not user:
+		return respond("Unknown user", code=404)
+
+	scores = Score.query.filter(Score.user == user).all()
+	activities = []
+
+	for score in scores:
+		activities.append({
+			'id': score.id,
+			'name': score.activity.name,
+			'start': time.mktime(score.start.timetuple()),
+			'end': time.mktime(score.end.timetuple()),
+			'points': score.activity.points,
+			'skill': score.activity.skill.name,
+			'completed': score.completed
+		})
+
+	return respond("SUCCESS", data={
+		'activities': activities
+	})
+
+@admin_v1.route('/getAllSessions', methods=['POST'])
+def v1_admin_getAllSessions():
+	try:
+		check_params(request, ['key'])
+
+		if request.form['key'] != app.config['ADMIN_SECRET_KEY']:
+			raise StandardError('Bad. Go away.')
+
+	except StandardError as e:
+		return respond(str(e), code=400), 400
+
+	sessions = []
+
+	for s in Session.query.all():
+		sessions.append({
+			#do nothing for now
+			})
+
+	return respond("SUCCESS", data={'sessions': user.getDict()})
+
+@admin_v1.route('/banUser', methods=['POST'])
+def v1_admin_banUser():
+	try:
+		check_params(request, ['key', 'uid'])
+
+		if request.form['key'] != app.config['ADMIN_SECRET_KEY']:
+			raise StandardError('Bad. Go away.')
+
+		uid = request.form['uid']
+	except StandardError as e:
+		return respond(str(e), code=400), 400
+
+	user = User.query.filter(User.id == uid).first()
+
+	if not user:
+		return respond("Unknown user", code=404)
+
+	#banUser
+
+	return respond("SUCCESS")
